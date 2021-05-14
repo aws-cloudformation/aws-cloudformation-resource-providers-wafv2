392b2fe44aededbacff8c1c0a3bc464689ea41d3
Aditya Vajjha <avajjha@amazon.com>
2021-05-14 15:54:45 +0800

    Cleaned up Converter

--- aws-wafv2-loggingconfiguration/src/main/java/software/amazon/wafv2/converters/Converter.java
+++ aws-wafv2-loggingconfiguration/src/main/java/software/amazon/wafv2/converters/Converter.java
@@ -8,8 +8,6 @@ import com.google.common.collect.ImmutableMap;
 
 import software.amazon.wafv2.loggingconfiguration.LoggingFilter;
 import software.amazon.awssdk.services.wafv2.model.All;
-import software.amazon.awssdk.services.wafv2.model.AllQueryArguments;
-import software.amazon.awssdk.services.wafv2.model.Body;
 import software.amazon.awssdk.services.wafv2.model.Method;
 import software.amazon.awssdk.services.wafv2.model.QueryString;
 import software.amazon.awssdk.services.wafv2.model.UriPath;
@@ -31,26 +29,6 @@ public interface Converter {
     // FieldToMatch
     software.amazon.awssdk.services.wafv2.model.FieldToMatch convert(FieldToMatch source);
     FieldToMatch invert(software.amazon.awssdk.services.wafv2.model.FieldToMatch source);
-
-    // All Query Arguments and Body are not supported in the Schema. We include this for MapStruct Conversion only 
-    default AllQueryArguments convertAllQueryArguments(Map<String, Object> value) {
-        return AllQueryArguments.builder().build();
-    }
-
-    // All Query Arguments and Body are not supported in the Schema. We include this for MapStruct Conversion only 
-    default Map<String, Object> invertAllQueryArguments(AllQueryArguments value) {
-        return (value == null) ? null : ImmutableMap.of();
-    }
-    
-    // All Query Arguments and Body are not supported in the Schema. We include this for MapStruct Conversion only    
-    default Body convertBody(Map<String, Object> value) {
-        return Body.builder().build();
-    }
-
-    // All Query Arguments and Body are not supported in the Schema. We include this for MapStruct Conversion only    
-    default Map<String, Object> invertBody(Body value) {
-        return (value == null) ? null : ImmutableMap.of();
-    }
 
     default UriPath convertUriPath(Map<String, Object> value) {
         return UriPath.builder().build();
